88aca7f82f158ed7e99e94bae9ca4e94c2669d11
Mufliha Safira <muflihasafira@gmail.com>
2019-02-27 20:55:46 +0700

    2 detik

--- Assets/Scenes/FadeInImage.cs
+++ Assets/Scenes/FadeInImage.cs
@@ -21,7 +21,7 @@ public class FadeInImage : MonoBehaviour
     }
     void FadeIn()
     {
-        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+        splashImage.CrossFadeAlpha(1.0f, 2.5f, false);
 
 
     }
