b44e6f8dc10423d93d54c0ff18fa3d1a0c19ef55
Mufliha Safira <muflihasafira@gmail.com>
2019-02-27 20:54:46 +0700

    fade in prolog

---
+++ Assets/Scenes/FadeInImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class FadeInImage : MonoBehaviour
+{
+    public Image splashImage;
+    public string loadLevel;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(4.5f);
+
+
+    }
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 1.5f, false);
+
+
+    }
+}
