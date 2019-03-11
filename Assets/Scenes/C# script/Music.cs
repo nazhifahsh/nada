ff4e8337f5ab3632f10245a6ae8f59b0a8b5fcaf
Mufliha Safira <muflihasafira@gmail.com>
2019-03-11 15:29:05 +0700

    music everywhere

---
+++ Assets/Scenes/C# script/Music.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Music : MonoBehaviour
+{
+    static Music instance = null;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ToggleSound()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+            //audiolistener.volume = 1;
+        }
+        else
+        {
+            PlayerPrefs.SetInt("muted", 0);
+            //audiolisteer.volume = 0
+        }
+    }
+}
