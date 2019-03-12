ffd015e3f530af9ed2bbb8de92def803af501557
Nazhifah <nazpriansyah@gmail.com>
2019-03-12 17:32:28 +0700

    heu

---
+++ Assets/Scenes/C# script/LevelControlScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelControlScript : MonoBehaviour
+{
+
+    public static LevelControlScript instance = null;
+  
+    int sceneIndex, levelPassed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+    }
+
+    public void youWin()
+    {
+        if (sceneIndex == 6)
+            Invoke("loadMainMenu", 1f);
+        else
+        {
+            if (levelPassed < sceneIndex)
+                PlayerPrefs.SetInt("LevelPassed", sceneIndex);
+            Invoke("loadNextLevel", 1f);
+        }
+    }
+
+
+    void loadNextLevel()
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+
+    void loadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
