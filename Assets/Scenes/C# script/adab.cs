2a81efd6f65833ea5e925e1b092770d29b667bee
Nazhifah <nazpriansyah@gmail.com>
2019-03-23 15:11:49 +0700

    ihmngyty

---
+++ Assets/Scenes/C# script/adab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adab : MonoBehaviour
+{
+    public Animator adabb;
+   
+    // Start is called before the first frame update
+    public void onclick()
+    {
+        bool oq = true;
+        adabb.SetBool("oq", true);
+       
+    }
+   
+   
+}
