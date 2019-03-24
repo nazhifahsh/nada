ca7b365600f3b0976006df51b6842eaa9a5a9d15
Mufliha Safira <muflihasafira@gmail.com>
2019-03-24 21:39:24 +0700

    qwergtrhy

--- Assets/Scenes/C# script/text.cs
+++ Assets/Scenes/C# script/text.cs
@@ -7,11 +7,21 @@ public class text : MonoBehaviour
 {
     public Animator anim;
     bool udah = false;
+    public Image tampil;
   
     public void udaah()
     {
         udah = true;
         anim.SetBool("udah", true);
     }
-   
+
+    public void uddah()
+    {
+        tampil.CrossFadeAlpha(1.0f, 1.5f, false);
+
+
+    }
+
 }
+
+
