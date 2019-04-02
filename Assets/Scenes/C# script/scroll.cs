655278d71e3cf4931d5ac722d8ea27196273170f
Nazhifah <nazpriansyah@gmail.com>
2019-04-02 22:26:13 +0700

    b

--- Assets/Scenes/C# script/scroll.cs
+++ Assets/Scenes/C# script/scroll.cs
@@ -36,6 +36,10 @@ public class scroll : MonoBehaviour
     {
         udel.SetBool("boollagi", true);
     }
+    public void serge()
+    {
+        udel.SetBool("boollagi", true);
+    }
     public void serr()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -47,6 +51,66 @@ public class scroll : MonoBehaviour
     public void yy()
     {
         udel.SetBool("onklik", true);
+    }
+    public void gg()
+    {
+        udel.SetBool("onkliklg", true);
+    }
+    public void zrr()
+    {
+        udel.SetTrigger("grr");
+    }
+    public void hoo()
+    {
+        udel.SetTrigger("lll");
+    }
+    public void ngok()
+    {
+        udel.SetTrigger("haa");
+    }
+    public void hy()
+    {
+        udel.SetTrigger("hii");
+    }
+    public void moo()
+    {
+        udel.SetBool("ko",true);
+    }
+    public void io()
+    {
+        udel.SetBool("io",true);
+    }
+    public void move1()
+    {
+        udel.SetBool("to1", true);
+    }
+    public void move2()
+    {
+        udel.SetBool("to2", true);
+    }
+    public void move3()
+    {
+        udel.SetBool("to3", true);
+    }
+    public void move4()
+    {
+        udel.SetBool("to4", true);
+    }
+    public void move5()
+    {
+        udel.SetBool("to5", true);
+    }
+    public void txt()
+    {
+        udel.SetBool("too", true);
+    }
+    public void ttxt()
+    {
+        udel.SetBool("tuu", true);
+    }
+    public void txt0()
+    {
+        udel.SetBool("tooo", true);
     }
 }
 
