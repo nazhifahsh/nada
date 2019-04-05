5fa2b4243d9c006406a697e71ea748b0aee1ba1e
Nazhifah <nazpriansyah@gmail.com>
2019-04-05 21:48:07 +0700

    yuu

--- Assets/Scenes/C# script/adab.cs
+++ Assets/Scenes/C# script/adab.cs
@@ -6,18 +6,42 @@ using UnityEngine.SceneManagement;
 public class adab : MonoBehaviour
 {
     public Animator adabb;
-    bool oq = false;
    
-
-
-   
-    // Start is called before the first frame update
-    public void onclick()
+    public void ad1()
     {
-        oq = true;
-        adabb.SetBool("oq", true);
-        imm.score += 1;
+        adabb.SetBool("ad1", true);
     }
-  
+    public void ad2()
+    {
+        adabb.SetBool("ad2", true);
+    }
+    public void ad3()
+    {
+        adabb.SetBool("ad3", true);
+    }
+    public void ad4()
+    {
+        adabb.SetBool("ad4", true);
+    }
+    public void ad5()
+    {
+        adabb.SetBool("ad5", true);
+    }
+    public void ad6()
+    {
+        adabb.SetBool("ad6", true);
+    }
+    public void ad7()
+    {
+        adabb.SetBool("sr", true);
+    }
+    public void ad8()
+    {
+        adabb.SetBool("et", true);
+    }
+    public void ad9()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
 }
