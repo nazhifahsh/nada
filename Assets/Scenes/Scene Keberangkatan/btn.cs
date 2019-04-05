71487995a08ba30f0c37170a08c7b091a42df0cd
Nazhifah <nazpriansyah@gmail.com>
2019-04-05 10:55:36 +0700

    aaa

---
+++ Assets/Scenes/Scene Keberangkatan/btn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btn : MonoBehaviour
+{
+    public Animator hey;
+
+   public void diz()
+    {
+        hey.SetBool("disable", true);
+    }
+}
