edf8929f04a5e3e293403563740bf5d6090145b4
Mufliha Safira <muflihasafira@gmail.com>
2019-05-07 22:39:29 +0700

    n

--- Assets/Scenes/C# script/Settings.cs
+++ Assets/Scenes/C# script/Settings.cs
@@ -2,17 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Settings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  public void msc()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
