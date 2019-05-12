b12c21db4fd64c2947cf61df684326e2e8f6098f
Nazhifah <nazpriansyah@gmail.com>
2019-05-13 01:06:58 +0700

    ngans abis

---
+++ Assets/Scenes/Prolog/home.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class home : MonoBehaviour
+{
+    public Animator pulang;
+
+    public void bck()
+    {
+        pulang.SetBool("back", true);
+    }
+    public void udh()
+    {
+        Time.timeScale = 1f;
+        pulang.SetBool("udh", true);
+    }
+    public void main()
+    {
+        pulang.SetBool("main", true);
+    }
+    public void stopp()
+    {
+        Time.timeScale = 0f;
+    }
+}
